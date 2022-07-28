39e1406e0ec874676ca52e775e47f7800b80d94d
Palina <palina.maslava@gmail.com>
2022-07-28 16:33:19 +0300

    refactor(path follower): look at next point on way

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -10,8 +10,7 @@ public class PathFollower : MonoBehaviour
 
     [SerializeField] private float _speed;
     [SerializeField] private PathCreator _pathCreator;
-
-
+	
     private Rigidbody _rigidbody;
     private float _distanceTraveled;
 	private void Start()
@@ -19,15 +18,16 @@ public class PathFollower : MonoBehaviour
 		_rigidbody = GetComponent<Rigidbody>();
 		_rigidbody.MovePosition(_pathCreator.path.GetPointAtDistance(_distanceTraveled));
 
+		transform.Rotate(0, 0, -90);
 	}
 
 	private void Update()
 	{
 		_distanceTraveled += Time.deltaTime * _speed;
 		Vector3 nextPoint = _pathCreator.path.GetPointAtDistance(_distanceTraveled, EndOfPathInstruction.Loop);
-		nextPoint.y = transform.position.y;
-		transform.LookAt(nextPoint);
+		nextPoint.y = _rigidbody.position.y;
 
+		_rigidbody.rotation = _pathCreator.path.GetRotationAtDistance(_distanceTraveled);
 		_rigidbody.MovePosition(nextPoint);
 	}
 }
